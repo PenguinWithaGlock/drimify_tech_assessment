c39b0867b483a40f8cbe2e133fecca09763ff8b4
Ewa <ewap2738@gmail.com>
2025-06-03 17:33:56 +0100

    dispaly
    simple ui design

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,60 +8,49 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class TaskController extends AbstractController
 {
     #[Route('/task', name: 'list_tasks', methods: ['GET'])]
-    public function list(TaskRepository $taskRepo): JsonResponse
+    public function list(TaskRepository $taskRepo): Response
     {
         $tasks = $taskRepo->findBy(['deletedAt' => null]);
-        $data = [];
 
-        foreach ($tasks as $task) {
-            $data[] = [
-                'id' => $task->getId(),
-                'title' => $task->getTitle(),
-                'isDone' => $task->getIsDone(),
-                'createdAt' => $task->getCreatedAt()?->format('Y-m-d H:i:s'),
-                'updatedAt' => $task->getUpdatedAt()?->format('Y-m-d H:i:s'),
-                'deletedAt' => $task->getDeletedAt()?->format('Y-m-d H:i:s'),
-            ];
-        }
-
-        return new JsonResponse($data);
+        return $this->render('task/index.html.twig', ['tasks' => $tasks]);
     }
 
     #[Route('/tasks', name: 'create_task', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
-        $data = json_decode($request->getContent(), true);
+        $title = $request->request->get('title');
 
         $task = new Task();
-        $task->setTitle($data['title']);
+        $task->setTitle($title);
         $em->persist($task); 
         $em->flush(); 
 
-        return new JsonResponse(['message' => 'Task created successfully!']);
+        return $this->redirectToRoute('list_tasks');
     }
 
     #[Route('/tasks/{id}/toggle', name: 'toggle_done_task', methods: ['POST'])]
-    public function toggleDone(Task $task, EntityManagerInterface $em): JsonResponse
+    public function toggleDone(Task $task, EntityManagerInterface $em): Response
     {
         $task->setIsDone(!$task->getIsDone());
         $task->setUpdatedAt(new \DateTime());
         $em->flush();
 
-        return new JsonResponse(['message' => 'Toggled task status',]);
+        return $this->redirectToRoute('list_tasks');
     }
 
     #[Route('/tasks/{id}/delete', name: 'delete_task', methods: ['POST'])]
-    public function toggleDelete(Task $task, EntityManagerInterface $em): JsonResponse
+    public function toggleDelete(Task $task, EntityManagerInterface $em): Response
     {
         $task->setDeletedAt(new \DateTime());
         $task->setUpdatedAt(new \DateTime());
         $em->flush();
 
-        return new JsonResponse(['message' => 'Task deleted successfully',]);
+        return $this->redirectToRoute('list_tasks');
     }
 }
